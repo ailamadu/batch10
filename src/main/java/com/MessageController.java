9537c6450df52d985981a5e7f56280bb7f53621b
root <root@build-server.us-central1-a.c.elated-chassis-309115.internal>
2021-06-02 07:14:08 +0000

    fixes

--- src/main/java/com/MessageController.java
+++ src/main/java/com/MessageController.java
@@ -3,8 +3,9 @@ package com;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class MessageController {
 	
 private static final Logger log = LoggerFactory.getLogger(MessageController.class);
